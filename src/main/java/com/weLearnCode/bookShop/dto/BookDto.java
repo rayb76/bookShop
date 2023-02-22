4c405efd9b41cfa9231258a0ebd5b1d772544f31
rayb76 <rayb76@outlook.com>
2023-02-21 16:49:37 -0800

    bookShop Initial commit

---
+++ src/main/java/com/weLearnCode/bookShop/dto/BookDto.java
@@ -0,0 +1,18 @@
+package com.weLearnCode.bookShop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDto {
+
+    private String title;
+
+
+}
+
